78d80be010ce93bcc35eb7248cab79653fefbd28
jstehlik <jstehlik@ohpen.com>
2022-07-26 14:05:29 +0200

    chore: LANZ-2766 Update variable descriptions

--- variables.tf
+++ variables.tf
@@ -7,15 +7,15 @@ variable "stage" {
   type        = string
   description = "Stage of the outputs, e.g. dev, tst, acc, prd"
 }
-
-variable "deployment" {
-  type        = string
-  default     = "main"
-  description = "Deployment of the resource, e.g. main, b, c, d. Used only if baseline is deployment-specific"
-}
+#
+#variable "deployment" {
+#  type        = string
+#  default     = "main"
+#  description = "Deployment of the resource, e.g. main, b, c, d. Used only if baseline is deployment-specific"
+#}
 
 variable "network" {
-  description = "Return outputs from network baseline"
+  description = "Enable outputs from network baseline. Override default stage and deployment if necessary."
   default     = {
     enable     = false
     stage      = null
@@ -24,7 +24,7 @@ variable "network" {
 }
 
 variable "ecs" {
-  description = "Return outputs from ECS baseline"
+  description = "Enable outputs from ECS baseline. Override default stage and deployment if necessary."
   default     = {
     enable     = false
     stage      = null
@@ -33,7 +33,7 @@ variable "ecs" {
 }
 
 variable "cloudmap" {
-  description = "Return outputs from CloudMap baseline"
+  description = "Enable outputs from CloudMap baseline. Override default stage and deployment if necessary."
   default     = {
     enable     = false
     stage      = null
